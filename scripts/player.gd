5779d9383d483bced92bd824b4e11619dfcb73bc
lehmius <lehmius@clajus.eu>
2025-06-06 12:06:33 +0200

    Added shoot signal emitting on InputEventKey.pressed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,26 @@
 extends CharacterBody2D
 
-@export var SPEED:int = 300
+signal shoot(projectile, pressedKey, global_position)
+
+@export var speed:int = 300
+
+var Projectile = preload("res://scenes/projectile.tscn")
+
+func _ready() -> void:
+	pass
+
+## Emits the shoot(projectile, pressedKey, global_position) signal on InputEventKey.pressed event.
+##
+## @param event: InputEvent
+## @returns: void
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		shoot.emit(Projectile, event.keycode, global_position)
+
+
+func _process(delta: float) -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
-	velocity.x = SPEED
+	velocity.x = speed
 	move_and_slide()
