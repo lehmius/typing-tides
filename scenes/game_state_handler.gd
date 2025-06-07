14bda2185feb52974dd90a6106b48a6dbda680d1
Falko Riedl <57110249+FalkoRiedl@users.noreply.github.com>
2025-06-07 23:54:42 +0200

    Add GameStateHandler scene structure

---
+++ scenes/game_state_handler.gd
@@ -0,0 +1,6 @@
+## This script is responsible for the core functionality of the gameplay loop.
+## It keeps track of the gamestate, instances enemies and handles failure states.
+extends Node
+
+func _ready() -> void:
+	pass
