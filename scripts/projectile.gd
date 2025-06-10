569396da43d24743199b30b3817ba7af4c0f2cfd
lehmius <lehmius@clajus.eu>
2025-06-10 12:13:47 +0200

    Added basic movement with default velocity of 0. Also hooked into screen exiting signal for projectile deletion.

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -1,6 +1,16 @@
+class_name Projectile
+
 extends Area2D
 
-@export var speed = Vector2.RIGHT
+var velocity:Vector2
+
+func _ready() -> void:
+	velocity = Vector2(0, 0)
 
 func _physics_process(delta: float) -> void:
-    position += speed * delta
+	position += velocity * delta
+
+## Projectiles delete themeselves as soon as they leave the screen.
+## This is a fallback option should they not be deleted prior to this.
+func _on_screen_existed() -> void:
+	queue_free()
