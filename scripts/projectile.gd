f92ffbc38b908b3a445c7aaee5ebd565093af283
lehmius <lehmius@clajus.eu>
2025-06-10 12:15:03 +0200

    Added rotation control functions to projectile.

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -14,3 +14,32 @@ func _physics_process(delta: float) -> void:
 ## This is a fallback option should they not be deleted prior to this.
 func _on_screen_existed() -> void:
 	queue_free()
+
+## Changes the rotation of the projectile using radians. 
+## Rotating also adjusts the direction in which the projectile travels.
+##
+## @param radians: The new rotation in radians
+##
+## @returns: void
+func rotate_using_radians(radians: float) -> void:
+	rotation += radians
+	velocity = velocity.rotated(radians)
+	print(velocity)
+
+## Changes the rotation of the projectile using degrees. 
+## Rotating also adjusts the direction in which the projectile travels.
+##
+## @param degree: The new rotation in degrees
+##
+## @returns: void
+func rotate_using_degrees(degree: float) -> void:
+	rotate_using_radians(deg_to_rad(degree))
+
+
+## Resets the visual and movement rotation to neutral.
+## For the movement rotation this means left to right movement.
+##
+## @returns: void
+func reset_rotation() -> void:
+	velocity = velocity.rotated(-rotation)
+	rotation = 0
