ad257884576e07abb4a84f39ad88c38182f66cb5
lehmius <lehmius@clajus.eu>
2025-06-16 14:25:50 +0200

    Refactored popups to utilize the pause_component.

--- scripts/popup.gd
+++ scripts/popup.gd
@@ -4,13 +4,14 @@ extends Control
 
 @export_multiline var text:String = ""
 @onready var rich_text_label:RichTextLabel = $Panel/VBoxContainer/MarginContainer/RichTextLabel
+@onready var pause_component:Node = $PauseComponent
 
 func _ready() -> void:
-	Engine.set_time_scale(0)
+	pause_component.pause()
 	set_text(text)
 
 func _on_button_button_down() -> void:
-	Engine.set_time_scale(1.0)
+	pause_component.resume()
 	queue_free() # Replace with function body.
 
 ## Sets the text of the popup panel to the text passed to the function
