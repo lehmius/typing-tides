654accb49763a5d20a95afe644bcb7051ef0a678
lehmius <lehmius@clajus.eu>
2025-06-16 07:59:06 +0200

    Added functionality to set text from the scene instantiating the popup.

--- scripts/popup.gd
+++ scripts/popup.gd
@@ -1,10 +1,17 @@
+class_name popupPanel
+
 extends Control
+
+@export_multiline var text:String = ""
+@onready var rich_text_label:RichTextLabel = $Panel/VBoxContainer/MarginContainer/RichTextLabel
 
 func _ready() -> void:
 	Engine.set_time_scale(0)
-
-
+	set_text(text)
 
 func _on_button_button_down() -> void:
 	Engine.set_time_scale(1.0)
 	queue_free() # Replace with function body.
+
+func set_text(text:String):
+	rich_text_label.append_text(text)
