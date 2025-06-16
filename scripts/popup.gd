62a676704ba39d0f7a8b7d5b6986e7fe8355686d
lehmius <lehmius@clajus.eu>
2025-06-16 15:06:12 +0200

    Refactored specific informational popup ui elements into seperate scene.

--- scripts/popup.gd
+++ scripts/popup.gd
@@ -2,22 +2,12 @@ class_name popupPanel
 
 extends Control
 
-@export_multiline var text:String = ""
-@onready var rich_text_label:RichTextLabel = $Panel/VBoxContainer/MarginContainer/RichTextLabel
 @onready var pause_component:Node = $PauseComponent
+@export var ui_element:Control
 
 func _ready() -> void:
 	pause_component.pause()
-	set_text(text)
 
 func _on_button_button_down() -> void:
 	pause_component.resume()
-	queue_free() # Replace with function body.
-
-## Sets the text of the popup panel to the text passed to the function
-##
-## @param text: Text to be displayed by the popup panel
-## @return: void
-func set_text(text:String) -> void:
-	rich_text_label.clear()
-	rich_text_label.append_text(text)
+	ui_element.queue_free() # Replace with function body.
