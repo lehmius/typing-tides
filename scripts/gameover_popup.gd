36e134c66c57f939ba5f40d7d16796b45191a502
lehmius <lehmius@clajus.eu>
2025-06-17 11:24:13 +0200

    Refactored RichTextLabel displaying player stats to be its own scene.

--- scripts/gameover_popup.gd
+++ scripts/gameover_popup.gd
@@ -2,19 +2,7 @@ extends Control
 
 @onready var restart_button:Button = $Popup/VBoxContainer/CenterContainer/HBoxContainer/MarginContainer/RestartButton
 @onready var menu_button: Button = $Popup/VBoxContainer/CenterContainer/HBoxContainer/MarginContainer2/MenuButton
-@onready var score_label: RichTextLabel = $Popup/VBoxContainer/score
 
 func _ready() -> void:
 	restart_button.set_levelID_to_current()
 	menu_button.set_scene_to_be_loaded(-2)
-	SignalBus.connect("displayPerformance", _on_playerPerformanceMetrics)
-
-## On receiving the player performance metrics this function inserts the stats
-## into a table in the score RichTextLabel.
-##
-## @param playerPerformanceMetrcis: Dictionary containing the player stats
-## @returns: void
-func _on_playerPerformanceMetrics(playerPerformanceMetrics: Dictionary) -> void:
-	score_label.text = """[table=3]
-	[cell][b]score[/b][/cell][cell][b]Zeichen/min[/b][/cell][cell][b]Genauigkeit[/b][/cell]
-	[/table]"""
---
+++ scripts/score_label.gd
@@ -0,0 +1,19 @@
+extends RichTextLabel
+
+func _ready() -> void:
+	SignalBus.connect("displayPerformance", _on_playerPerformanceMetrics)
+
+
+## On receiving the player performance metrics this function inserts the stats
+## into a table in the score RichTextLabel.
+##
+## @param playerPerformanceMetrcis: Dictionary containing the player stats
+## @returns: void
+func _on_playerPerformanceMetrics(playerPerformanceMetrics: Dictionary) -> void:
+	append_text("""[center][table=2]
+	[cell][left]score[/left][/left][/cell][cell][left]{score}[/left][/cell]
+	[cell][left]Zeichen/min[/left][/cell][cell][left]{charactersPerMinute}[/left][/cell]
+	[cell][left]Genauigkeit[/left][/cell][cell][left]{accuracy}[/left][/cell]
+	[cell][left]Beste Streak[/left][/cell][cell][left]{highestConsecutiveStreak}[/left][/cell]
+	[cell][left]Zeit[/left][/cell][cell][left]{time}[/left][/cell]
+	[/table][center]""".format(playerPerformanceMetrics))
