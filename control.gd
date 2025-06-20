ba03b04232b36ff47ddeee0294977fac44c25c2e
Falko Riedl <57110249+FalkoRiedl@users.noreply.github.com>
2025-06-20 23:28:17 +0200

    Add scrolling background screne

--- control.gd
+++ control.gd
@@ -1,21 +1,33 @@
 extends Control
 
-var movementSpeed:int = 150 #Movement speed of the background in pixels/second
+@export var movementSpeed:int = 50 #Movement speed of the background in pixels/second
 
 @onready var bg1:TextureRect=$bg1
 @onready var backgrounds=[bg1]
-var textureHeight=1280
 
+var textureHeight=1280
+var textureWidth=640
+
+var maxOffset=1280-360		# Maximum offset before the end of the image becomes visible
+var safetyOffset=50			# Safety offset to guarantee instancing happens offcamera
 var scrollingBGTexture=load("res://assets/levels/background/endless_loop.png")
 
+# Instances backgrounds as needed to give permanent scrolling effect
 func _physics_process(delta: float) -> void:
 	for picture in backgrounds:
 		picture.position.y-=movementSpeed*delta
+	if backgrounds[0].position.y<-(maxOffset) and backgrounds.size()<2:
+		backgrounds+=[createNewBackground(backgrounds[0].position.y+1280)]
+	if backgrounds[0].position.y<-textureHeight:
+		backgrounds[0].queue_free()
+		backgrounds.remove_at(0)
+
+
 
 ## Creates a new background instance
 ##
 ## @returns: Reference to TextureRectNode
-func createNewBackground(yposition=):
+func createNewBackground(yposition=0):
 	var backgroundRect = TextureRect.new()
 	backgroundRect.texture=scrollingBGTexture
 	add_child(backgroundRect)
