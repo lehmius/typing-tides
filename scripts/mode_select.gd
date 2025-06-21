cbe6b23d34fd5bfd3c0493a4be7f802305342449
lehmius <lehmius@clajus.eu>
2025-06-21 09:28:21 +0200

    Added introductory informational popup to mode select.

--- scripts/mode_select.gd
+++ scripts/mode_select.gd
@@ -1,7 +1,9 @@
 extends Control
 
+@onready var popup = $InformationalPopup
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
+	if popup == null and event is InputEventMouseButton and event.is_pressed():
 		if event.position.x<=get_viewport().get_visible_rect().size.x/2:
 			SignalBus.loadLevel.emit(-2)
 		else:
