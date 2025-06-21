d755770f13b6e53063f74ae31e65a8b335411f65
lehmius <lehmius@clajus.eu>
2025-06-21 11:12:14 +0200

    Added popups setting global state to paused/unpaused on instantiation/freeing

--- scripts/popup.gd
+++ scripts/popup.gd
@@ -5,5 +5,9 @@ extends Control
 @onready var close_button: Button = $Panel/Button
 @export var ui_element:Control
 
+func _ready() -> void:
+	GlobalState.setPaused()
+	
 func _on_button_button_down() -> void:
+	GlobalState.setUpaused()
 	ui_element.queue_free()
