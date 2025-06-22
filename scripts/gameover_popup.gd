cd3a99342ae8b92778a825c284dd31983ca950ab
lehmius <lehmius@clajus.eu>
2025-06-22 19:36:56 +0200

    Added level indicator to level end screens

--- scripts/gameover_popup.gd
+++ scripts/gameover_popup.gd
@@ -2,8 +2,13 @@ extends Control
 
 @onready var restart_button:Button = $Popup/VBoxContainer/CenterContainer/HBoxContainer/MarginContainer/RestartButton
 @onready var menu_button: Button = $Popup/VBoxContainer/CenterContainer/HBoxContainer/MarginContainer2/MenuButton
+@onready var heading: RichTextLabel = $Popup/VBoxContainer/MarginContainer/RichTextLabel
 
 func _ready() -> void:
+	if GlobalState.getLevelID() > 0:
+		heading.text = "Wie schade... Level %s nicht geschafft" % str(GlobalState.getLevelID())
+	else:
+		heading.text = "GAME OVER"
 	restart_button.set_levelID_to_current()
 	menu_button.set_scene_to_be_loaded(-3)
 	SignalBus.deleteMenus.connect(deleteSelf)
--- scripts/levelover_popup.gd
+++ scripts/levelover_popup.gd
@@ -3,8 +3,12 @@ extends Control
 @onready var restart_button:Button = $Popup/VBoxContainer/CenterContainer/HBoxContainer/MarginContainer/RestartButton
 @onready var menu_button: Button = $Popup/VBoxContainer/CenterContainer/HBoxContainer/MarginContainer2/MenuButton
 @onready var next_level_button: Button = $Popup/VBoxContainer/CenterContainer/HBoxContainer/MarginContainer3/NextLevelButton
+@onready var heading: RichTextLabel = $Popup/VBoxContainer/MarginContainer/RichTextLabel
 
 func _ready() -> void:
+	if GlobalState.getLevelID() > 0:
+		heading.text = "Juhu! Level %s geschafft" % str(GlobalState.getLevelID())
+	heading.text = heading.text.format(GlobalState.getLevelID())
 	restart_button.set_levelID_to_current()
 	menu_button.set_scene_to_be_loaded(-3)
 	next_level_button.set_scene_to_be_loaded(int(GameStateHandler.getCurrentLevel()+1))
